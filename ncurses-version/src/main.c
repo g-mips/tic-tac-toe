fabaacfdd1405cd08b4196d78970cc3eea4ca7c4
g-mips <g-mips@users.noreply.github.com>
2020-05-17 16:56:22 -0600

    Get player's names

--- ncurses-version/src/main.c
+++ ncurses-version/src/main.c
@@ -5,6 +5,8 @@
 #include "player.h"
 #include "board.h"
 
+#define PLAYER_MAX_STR_SIZE 20
+
 int
 main(int argc, char ** argv)
 {
@@ -13,7 +15,13 @@ main(int argc, char ** argv)
     cbreak();
 
     refresh();
-    player_get_info((LINES / 2), (COLS / 2));
+
+    char player_one_name[PLAYER_MAX_STR_SIZE] = "";
+    char player_two_name[PLAYER_MAX_STR_SIZE] = "";
+    player_get_info((LINES / 2), (COLS / 2),
+        player_one_name, sizeof(player_one_name), 1);
+    player_get_info((LINES / 2), (COLS / 2),
+        player_two_name, sizeof(player_two_name), 2);
 
     board_init();
     board_setup();
--- ncurses-version/src/player.c
+++ ncurses-version/src/player.c
@@ -3,23 +3,33 @@
 #include "player.h"
 
 void
+player_input_choice()
+{
+}
+
+void
 player_get_info(
     int middle_screen_y,
-    int middle_screen_x)
+    int middle_screen_x,
+    char *player_name, size_t player_name_size,
+    unsigned int player_num)
 {
     WINDOW *player_info_window = newwin(
         20, 90, (middle_screen_y / 2), (middle_screen_x / 2));
+    keypad(player_info_window, TRUE);
 
     // Create border
     box(player_info_window, 0, 0);
 
     // Create prompt
     mvwprintw(player_info_window, 10, 22,
-        "Please enter player one's name: ");
+        "Please enter player %u's name: ", player_num);
 
     // Show these things
+    echo();
     wrefresh(player_info_window);
-    getch();
+
+    wgetnstr(player_info_window, player_name, player_name_size);
 
     // Clear the screen, show the clearing, and get rid of the window
     wclear(player_info_window);
